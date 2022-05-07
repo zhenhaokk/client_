418d1c935e59d34c2086377cf245f85cdbc5a278
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-07 21:37:57 +1000

    part 1

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,15 +1,27 @@
-export default (posts = [], action) => { 
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
+
+export default (posts = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
+    case FETCH_ALL:
       return action.payload;
-    case 'CREATE':
+    case CREATE:
       return [...posts, action.payload];
-    case 'UPDATE':
-      return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-    case 'DELETE':
+    case UPDATE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
-    case 'LIKE':
-      return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+    case LIKE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
 
     default:
       return posts;
